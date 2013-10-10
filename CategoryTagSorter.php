08dd4e8ccbc801ef1fea1cb6935878efc7c7c5c9
AlephNull <drenfro@vistaprint.com>
2013-10-10 15:39:31 -0400

    Updating legal name of Vistaprint
    Change-Id: Idf2df9dedfd39be757898b72ab85f40c1ddbbd5c

--- CategoryTagSorter.php
+++ CategoryTagSorter.php
@@ -10,7 +10,7 @@
  * @author Daniel Renfro
  *
  *
- * Copyright 2013 Vistaprint, Inc.
+ * Copyright 2013 Vistaprint Schweiz GmbH.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- CategoryTagSorter_body.php
+++ CategoryTagSorter_body.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright 2013 Vistaprint, Inc.
+ * Copyright 2013 Vistaprint Schweiz GmbH.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
